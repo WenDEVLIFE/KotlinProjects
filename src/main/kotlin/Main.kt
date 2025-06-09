d38ca2b8ca620f78b930c6c74a9a24604eb039c2
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-06-09 14:03:53 +0800

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    val name = "Wen"
+    val number = 42
+    val char = 'A'
+    val pi = 3.14
+
+    val message: String = "Hello Cat"
+    val count: Int = 1
+    val pi1: Double = 3.44;
+    val isTrue: Boolean = true
+
+    // Convert types
+    val number2: Int = 100
+    val number3 : Double = number2.toDouble()
+    val chardata: Char = 'B'
+    val toString: String = chardata.toString()
+
+
+    //  Display the message
+    println("Hello, World!")
+    println(message)
+    println(count)
+    println(char)
+    println(pi1)
+    print(toString)
+
+
+
+}
